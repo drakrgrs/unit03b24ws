3a8f9c637b33d6673e9cedfbf80cc5f8b231999a
Drake Rogers <drakrgrs@gmail.com>
2024-08-20 10:02:44 -1000

    all except css

--- src/App.jsx
+++ src/App.jsx
@@ -6,14 +6,30 @@ import { puppyList } from "./data.js";
 
 function App() {
   const [puppies, setPuppies] = useState(puppyList);
-  console.log("puppyList: ", puppyList);
+  const [featPupId, setFeatPupId] = useState(null);
+  const featPup = puppies.find((pup) => pup.id === featPupId);
+
   return (
     <>
+      {featPupId && (
+        <div className="fPup">
+          <h2>{featPup.name}</h2>
+          <ul>
+            <li>Age: {featPup.age}</li>
+            <li>Email: {featPup.email}</li>
+          </ul>
+        </div>
+      )}
       {puppies.map((puppy) => {
-        return <p key={puppy.id}>{puppy.name}</p>;
+        return (
+          <div>
+            <h2 onClick={() => setFeatPupId(puppy.id)} key={puppy.id}>
+              {puppy.name}
+            </h2>
+          </div>
+        );
       })}
     </>
   );
 }
-
 export default App;
